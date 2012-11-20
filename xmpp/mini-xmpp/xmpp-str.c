35a2d44d6b7c73c6cec3e9764783671b4032ff47
yu <yu@asterisk11.(none)>
2012-11-20 17:59:13 +0800

    fix register-str

--- xmpp/mini-xmpp/xmpp-str.c
+++ xmpp/mini-xmpp/xmpp-str.c
@@ -1,3 +1,4 @@
 const char *open_str="<stream:stream to=\"192.168.1.127\" xmlns=\"jabber:client\" xmlns:stream=\"http://etherx.jabber.org/streams\" version=\"1.0\">";
 
-const char *register_str="<msg><type>register</type><IDcode>idnumber</IDcode><password>211</password><username>211</username></msg>";
+const char *register_str="<auth mechanism=\"DIGEST-MD5\" xmlns=\"urn:ietf:params:xml:ns:xmpp-sasl\"/>";
+
