3b49a9ff9f8a9a4ca6a1a3d40e1119febd77ade1
yu <yu@asterisk11.(none)>
2012-11-21 14:32:13 +0800

    调整文件

---
+++ xmpp/mini-xmpp/mini-xmpp.h
@@ -0,0 +1,13 @@
+#ifndef __MINI_XMPP_H
+#define __MINI_XMPP_H
+
+#define SERVER_IP		"192.168.1.127"
+#define SERVER_PORT	5222
+
+#define JID_NAME		"211"
+#define JID_PSWD		"211"
+#define RESOURCE		"mini-xmmp"
+
+#define BUFSIZE		1024
+
+#endif
