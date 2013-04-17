01e1edc18bcedd1135399aee728ebea07e5eba89
yumm <yumm007@gmail.com>
2013-04-17 14:59:56 +0800

    命令中加上双引号

--- C/sqlite/sql.sh
+++ C/sqlite/sql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -e 's/-//g' $1 | awk '{printf "insert into TagUploadInfo(Barcode,EslId,Complete) VALUES('%s','%s',0);\n",  $1, substr($2, 1, 6)}' | sqlite3 $2
+sed -e 's/-//g' $1 | awk '{printf "insert into TagUploadInfo(Barcode,EslId,Complete) VALUES(\"%s\",\"%s\",0);\n",  $1, substr($2, 1, 6)}' | sqlite3 $2
 
