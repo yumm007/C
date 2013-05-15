8bbd9aa6c7725af0da22c8df27b59e7ea3be0637
yumm <yumm007@gmail.com>
2013-05-15 14:19:17 +0800

    扩展b2c，使其支持从标准输入读入数据

--- C/ascii2dot/protocal_data/r.sh
+++ C/ascii2dot/protocal_data/r.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 gcc ascii2dot.c protocal_pack.c -o ascii2dot -Wall
-#gcc protocal_pack.c -o protocal_pack -Wall
-./ascii2dot 172 72 > dot.bin
-b2c dot.bin > dot.c
-rm dot.bin
+./ascii2dot 172 72 | b2c > dot.c
 
