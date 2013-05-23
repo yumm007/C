a3ac99074d3f7dd1c43e8d4f3efbb0c46393e618
yumm <yumm007@gmail.com>
2013-05-23 15:06:59 +0800

    建立 pc 程序

---
+++ C/ap_demo/datatype.h
@@ -0,0 +1,78 @@
+#ifndef __DATA_TYPE_H
+#define __DATA_TYPE_H
+
+#include <stdint.h>
+
+#pragma pack(1)
+/** 网络数据包格式  */
+typedef struct HS_PKT_T {
+
+	/** 数据包头  */
+	struct HS_PKT_H {
+		uint16_t version;
+		uint16_t version_s;
+		uint8_t version_str[8];
+
+		uint16_t opcode;
+		uint16_t opcode_s;
+
+		uint16_t para;
+		uint16_t para_s;
+		
+		uint32_t lenth;
+		uint32_t lenth_s;
+
+		uint16_t reserved0;
+		uint16_t reserved1;
+
+	} header;
+
+	/** 数据包内容  */
+	union {
+		struct HS_PKT_OP_WRITEDATA {
+			uint8_t ctrl;
+			uint8_t para;
+			uint8_t powermode;
+			uint32_t wakeup_id;
+			uint8_t rf_ch;
+			uint16_t data_esl_num;
+			uint16_t sleep_esl_num;
+
+			//不定长的终端数据
+			struct data_area_t {
+				uint32_t id;
+				uint32_t len;
+				uint32_t data[0];
+			} data[0];
+			struct sleep_area_t {
+				uint32_t id;
+			} sleep[0];
+		} write;
+
+		struct HS_PKT_OP_KICKOFF {
+			uint8_t d;
+		} kickoff;
+
+		struct HS_PKT_OP_CANCL {
+			uint8_t d;
+		} cancel;
+
+		struct HS_PKT_OP_PING {
+			uint8_t d;
+		} ping;
+
+		struct HS_PKT_OP_ACK {
+			uint8_t d;
+		} ack;
+
+		struct HS_PKT_OP_NACK {
+			uint8_t d;
+		} nack;
+	} buf;
+	
+
+} HS_PKT_T;
+
+#pragma pack()
+
+#endif
---
+++ C/ap_demo/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <arpa/inet.h>
+
+static int my_connect(int sd, const char *addr, int port){
+	struct sockaddr_in svr;
+
+	bzero(&svr, sizeof(svr));
+	svr.sin_family = AF_INET;
+	svr.sin_addr.s_addr = inet_addr(addr);
+	svr.sin_port = htons(port);
+
+	if (connect(sd, (void *)&svr, sizeof(svr)) == -1) {
+		perror("connect:");
+		return -1;
+	}
+
+	return 0;
+}
+
+
+int socket_open(const char *addr, int port) {
+	int sd;
+
+	if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		perror("socket:");
+		goto _exit;
+	} 
+
+	if (my_connect(sd, addr, port) == -1)
+		goto _close;
+	
+	return sd;
+
+_close:
+	close(sd);
+_exit:
+	return sd;
+}
+
+#include "datatype.h"
+
+int main(void) {
+	int sd;
+	char bufs[1024];
+	HS_PKT_T *pkt =(void *)bufs;
+
+	if ((sd = socket_open("127.0.0.1", 21)) == -1)
+		return -1;
+	
+	read(sd, &pkt->buf.write, 1024);
+
+
+	printf("%s\n", (char *)&pkt->buf.write);
+
+	//my_connect(sd, "127.0.0.1", 22);
+	shutdown(sd, SHUT_RDWR);
+	//my_connect(sd, "127.0.0.1", 22);
+	
+	close(sd);
+	return 0;
+}
